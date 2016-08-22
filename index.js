4f886c34c0cde5dcba1fe248ccabdd9e9267b4a9
Andrew <andrewday40@gmail.com>
2016-08-22 19:04:16 -0400

    installed mongoose and added som folders

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var server = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 8080;
+mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
+mongoose.connect(mongoURI);
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
