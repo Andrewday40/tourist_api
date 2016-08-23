4929e087dfb34bd2c6191e3aa83c43ab2d9271aa
Andrew <andrewday40@gmail.com>
2016-08-22 20:32:27 -0400

    added a get, get id, and post route to POST

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var router = express.Router();
-var Post = require('..models/post.js');
+var Post = require('../models/post.js');
 
 router.get('/posts', getAllThePost);
 router.get('/posts/:id', getPost);
@@ -11,8 +11,51 @@ router.delete('/posts/:id', deletePost);
 
 module.exports = router;
 
-function getAllThePost(){}
-function getPost(){}
-function createPost(){}
+function getAllThePost(req, res, next){
+  Post.find({}, function(err, foundPosts){
+    if(err){
+      res.status(500).json({
+        msg:err
+      });
+    } else {
+      res.status(200).json({
+        posts: foundPosts
+      });
+    }
+  });
+}
+function getPost(req, res, next){
+  Post.find({_id: req.params.id}, function(err, foundAPost){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        post: foundAPost
+      });
+    }
+  });
+}
+function createPost(req, res, next){
+  var post = new Post({
+    city: req.body.city,
+    location: req.body.location,
+    description: req.body.description,
+    person: req.body.person,
+    catagory: req.body.catagory
+  });
+  post.save(function(err, newPost){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        post: newPost
+      });
+    }
+  });
+}
 function updatePost(){}
 function deletePost(){}
